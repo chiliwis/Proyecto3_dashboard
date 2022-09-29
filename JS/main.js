8cfc32e447a8d86bf79618c2b6cc446122004833
chiliwis <cristinasanchezg@gmail.com>
2022-09-29 13:39:17 -0500

    grafica se muestra

--- JS/main.js
+++ JS/main.js
@@ -5,64 +5,55 @@ const tblAnimales = document.getElementById("tblAnimales");
 const grafica = document.getElementById('myChart').getContext('2d');
 
 function cargarAnimales() {
-    fetch(base_url,
-        {
-            method: "GET"
+  fetch(base_url,
+    {
+      method: "GET"
+    }
+  )
+    .then(response => response.json())
+    .then(result => {
+      console.log(result);
+      tblAnimales.innerHTML = "";
+
+      const ctx = document.getElementById('myChart').getContext('2d');
+      const myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+          labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+          datasets: [{
+            label: 'Tiempo de Vida',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+              'rgba(255, 99, 132, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(153, 102, 255, 0.2)',
+              'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+              'rgba(255, 99, 132, 1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(75, 192, 192, 1)',
+              'rgba(153, 102, 255, 1)',
+              'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+          }]
+        },
+        options: {
+          scales: {
+            y: {
+              beginAtZero: true
+            }
+          }
         }
-    )
-        .then(response => response.json())
-        .then(result => {
-            console.log(result);
-            tblAnimales.innerHTML = "";
+      });
 
-            const config = {
-                type: 'radar',
-                data: result,
-                options: {
-                  elements: {
-                    line: {
-                    
-                      borderWidth: 3
-                    }
-                  }
-                },
-              };
-
-              const etiquetas = {
-                labels: [
-                  'Nombre',
-                  'Tiempo de Vida',
-                  'Peso Min',
-                  'Peso Max',
-                  'Ubicacion',
-                  'Genero'
-                ],
-                datasets: [{
-                  label: 'Nombre',
-                  data: [result],
-                  fill: true,
-                  backgroundColor: 'rgba(255, 99, 132, 0.2)',
-                  borderColor: 'rgb(255, 99, 132)',
-                  pointBackgroundColor: 'rgb(255, 99, 132)',
-                  pointBorderColor: '#fff',
-                  pointHoverBackgroundColor: '#fff',
-                  pointHoverBorderColor: 'rgb(255, 99, 132)'
-                }, {
-                  label: 'Tiempo de Vida',
-                  data: [result],
-                  fill: true,
-                  backgroundColor: 'rgba(54, 162, 235, 0.2)',
-                  borderColor: 'rgb(54, 162, 235)',
-                  pointBackgroundColor: 'rgb(54, 162, 235)',
-                  pointBorderColor: '#fff',
-                  pointHoverBackgroundColor: '#fff',
-                  pointHoverBorderColor: 'rgb(54, 162, 235)'
-                }]
-              };
-
-
-            for (const animales of result) {
-                let tr = `<tr>
+     
+      for (const animales of result) {
+        let tr = `<tr>
                 <td>${animales.id}</td>
                 <td>${animales.latin_name}</td>
                 <td>${animales.lifespan}</td>
@@ -72,12 +63,12 @@ function cargarAnimales() {
                 <td>${animales.geo_range}</td>
            
                 </tr>`;
-                tblAnimales.innerHTML += tr;
-            }
-        })
-        .catch((error) => {
-            console.log("Error al cargar animales");
-        });
+        tblAnimales.innerHTML += tr;
+      }
+    })
+    .catch((error) => {
+      console.log("Error al cargar animales");
+    });
 }
 // function generateRandomNumber (min=0, max=200) {
 //     let rand = Math.random();
